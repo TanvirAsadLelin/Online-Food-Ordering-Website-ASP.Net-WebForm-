8193f5324b59e4c10c6b8dba6125dd7c318139d9
Md Tanvir Asad <md.tanvirasad.lelin@gmail.com>
2022-08-15 20:45:21 +0600

    Working on Category In Admin Panel

---
+++ Foodie/Foodie/Admin/Category.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Foodie.Admin
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+            
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
